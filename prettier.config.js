1dd4d01245b9e683a87d72f9c281cc452d5570ed
Tamas Besenyei <tamas@besenyei.net>
2018-10-30 17:50:47 +0100

    init

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: false
+};
+
+module.exports = CONFIG;
